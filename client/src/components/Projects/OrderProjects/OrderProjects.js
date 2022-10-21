e2f5e1752700db770022e32f1cd820464029e7cc
theRobertSan <robertsan.pro@gmail.com>
2022-10-21 14:18:12 +0100

    Implement sorting feature for projects
    - Implement OrderProjects component where projects are sorted
    - Implement several types of sorting

---
+++ client/src/components/Projects/OrderProjects/OrderProjects.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { TextField, MenuItem } from "@mui/material";
+
+const OrderProjects = ({ originalProjects, projects, setProjects }) => {
+  const [sortMode, setSortMode] = useState(0);
+
+  // If redux projects are changed, sort again
+  useEffect(() => {
+    sortBy(sortMode, originalProjects);
+  }, [originalProjects]);
+
+  // Mode of sorting changed, sort again
+  const handleChange = (event) => {
+    const selectedSortMode = event.target.value;
+    setSortMode(selectedSortMode);
+    sortBy(selectedSortMode, projects);
+  };
+
+  const sortBy = (mode, projectsToSort) => {
+    const sortedProjects = [...projectsToSort];
+
+    switch (mode) {
+      case 0:
+        sortedProjects.sort((projectA, projectB) => Number(new Date(projectA.dueDate)) - Number(new Date(projectB.dueDate)));
+        break;
+      case 1:
+        sortedProjects.sort((projectA, projectB) => Number(new Date(projectB.dueDate)) - Number(new Date(projectA.dueDate)));
+        break;
+      case 2:
+        sortedProjects.sort((projectA, projectB) => projectB.importance - projectA.importance);
+        break;
+      default:
+        sortedProjects.sort((projectA, projectB) => projectA.importance - projectB.importance);
+    }
+    console.log(sortedProjects);
+    setProjects(sortedProjects);
+  };
+
+  return (
+    <>
+      <TextField select value={sortMode} label="Sort By" onChange={handleChange}>
+        <MenuItem value={0}>Closest Date</MenuItem>
+        <MenuItem value={1}>Latest Date</MenuItem>
+        <MenuItem value={2}>Hardest first</MenuItem>
+        <MenuItem value={3}>Easiest first</MenuItem>
+      </TextField>
+    </>
+  );
+};
+
+export default OrderProjects;
--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,18 +1,22 @@
-import { Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Grid, Stack, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 
 import Project from "./Project/Project";
 import useStyles from "./styles.js";
 import useGlobalStyles from "../../globalStyles";
+import OrderProjects from "./OrderProjects/OrderProjects";
 
 const Projects = () => {
   const classes = useStyles();
   const global = useGlobalStyles();
 
   const projects = useSelector((state) => state.projects);
-  const sortedProjects = projects.sort((projectA, projectB) => Number(new Date(projectA.dueDate)) - Number(new Date(projectB.dueDate)));
+  console.log(projects);
 
-  const projectItems = sortedProjects.map((project) => (
+  const [sortedProjects, setSortedProjects] = useState([]);
+
+  const projectItems = sortedProjects?.map((project) => (
     <Grid item xs={3}>
       <Project project={project}></Project>
     </Grid>
@@ -20,9 +24,12 @@ const Projects = () => {
 
   return (
     <div className={classes.projectsBox}>
-      <Typography className={classes.title} variant="h4">
-        My Projects
-      </Typography>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography className={classes.title} variant="h4">
+          My Projects
+        </Typography>
+        <OrderProjects originalProjects={projects} projects={sortedProjects} setProjects={setSortedProjects} />
+      </Stack>
       <Grid container justifyContent="flex-start" alignItems="center" rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
         {projectItems}
       </Grid>
